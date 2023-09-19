e07d26dd7b0789be574989f3520c6a489a61f855
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2023-09-20 00:53:42 +0700

    delete navigate

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:demo_ft_widget/effect_loading/app.dart';
-import 'package:demo_ft_widget/navigate/materialPageRoute/push_pop.dart';
-import 'package:demo_ft_widget/navigate/pushNamed/pushNamed.dart';
+
 
 import 'package:demo_ft_widget/widget/widget_main.dart'; // File chứa tất cả các demo Widget
 
@@ -9,8 +8,7 @@ import 'package:demo_ft_widget/view-library/tin_coder_view.dart';
 
 void main() async {
   // runApp(const MyApp());
-  // runApp(const PushPop());
-  // runApp(const PushNamed());
+
   runApp(const TinCoderMainView()); // tin_coder_view -- view-library
   // runApp(const DemoWidget());    // folder widget
 }
